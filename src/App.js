ec232f948ed917f4951c54ff5090ec5a9139f7b8
Heidi Patja <hpatja@gmail.com>
2020-09-06 22:08:11 +0200

    Added some style and final touches

--- src/App.js
+++ src/App.js
@@ -4,32 +4,41 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import Me from './components/Me.js';
 import Reports from './components/Reports.js';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBookOpen } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Hem</Link>
-          </li>
-          <li>
-            <Link to="/reports/week/1">Redovisning</Link>
-          </li>
-        </ul>
-      </nav>
-      {/* A <Switch> looks through its children <Route>s and
-  renders the first one that matches the current URL. */}
-      <Switch>
-          <Route exact path="/" component={Me}/>
-          <Route path="/reports/week/1" component={Reports}/>
-      </Switch>
-    </div>
-    <footer>
-        <p>&copy; Heidi Patja 2020</p>
-    </footer>
-  </Router>
+    <Router>
+        <div className="App">
+            <nav class="top-nav">
+                <div class="site-name">
+                    jsramverk
+                </div>
+            </nav>
+            <nav class="bottom-nav">
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <FontAwesomeIcon icon={faHome} />
+                            Hem</Link>
+                    </li>
+                    <li>
+                        <Link to="/reports/week/1">
+                            <FontAwesomeIcon icon={faBookOpen} />
+                            Redovisning</Link>
+                    </li>
+                </ul>
+            </nav>
+          {/* A <Switch> looks through its children <Route>s and
+      renders the first one that matches the current URL. */}
+          <Switch>
+              <Route exact path="/" component={Me}/>
+              <Route path="/reports/week/1" component={Reports}/>
+          </Switch>
+        </div>
+    </Router>
 );
 
 export default App;
--- src/components/Me.js
+++ src/components/Me.js
@@ -6,9 +6,9 @@ const Me = () => {
         <main>
             <h1>Me-app i jsramverk</h1>
 
-            <p>Jag heter Heidi Patja och är bosatt i Sundbyberg och läser därmed utbildningen på distans.</p>
+            <p>Jag heter Heidi Patja och är bosatt i Sundbyberg.</p>
 
-            <p>Vanligtvis är jag en ganska aktiv person, som bland annat ägnar mycket tid åt vandring och cykling, men just nu har jag en gipsad högerarm vilket gör livet lite mer begränsat. Det leder också till att denna beskrivning blir kortare än vanligt. Vi sparar bokstäverna till kodandet istället.</p>
+            <p>Vanligtvis är jag en ganska aktiv person, som bland annat ägnar mycket tid åt vandring och cykling, men just nu har jag en gipsad högerarm vilket gör livet lite mer begränsat. Det leder också till att denna beskrivning blir kort. Vi sparar bokstäverna till kodandet istället.</p>
         </main>
     );
 };
