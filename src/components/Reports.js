c88a603b81daeaf9b97b6dcaa8f2a7785bf11c44
Heidi Patja <hpatja@gmail.com>
2020-09-09 13:06:36 +0200

    Change to nested routes

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 // import ReadMe from './reports/README.md';
-import { BrowserRouter as Switch, Route, Link, useParams, useRouteMatch } from "react-router-dom";
+import { BrowserRouter as Switch, Route, Link, useRouteMatch } from "react-router-dom";
 
 function Reports() {
 
@@ -24,7 +24,7 @@ function Reports() {
                     <Route exact path={path}>
 
                     </Route>
-                    <Route path={`${path}/week/:kmom`} component={Report}>
+                    <Route path={`${path}/week/:week`} component={Report}>
                     </Route>
                 </Switch>
             </div>
@@ -32,15 +32,18 @@ function Reports() {
     );
 }
 
+
 class Report extends Component {
 
     constructor() {
         super();
-        this.state = { presentation: '' };
+        this.state = { res: '' };
     }
 
     callAPI() {
-        fetch("http://localhost:1337/reports/week/1")
+        const week = this.props.match.params.week;
+
+        fetch("http://localhost:1337/reports/week/" + week)
             .then(res => res.json())
             .then(res => this.setState({ week: res.data.week, content: res.data.content }));
     }
@@ -49,11 +52,17 @@ class Report extends Component {
         this.callAPI();
     }
 
-    render() {
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.week !== this.props.match.params.week) {
+            console.log("hej hej");
+            this.callAPI();
+        }
+    }
 
+    render() {
         return (
             <div>
-                <h3>Vecka {this.state.week}</h3>
+                <h2>Vecka {this.state.week}</h2>
                 <p>{this.state.content}</p>
             </div>
         );
